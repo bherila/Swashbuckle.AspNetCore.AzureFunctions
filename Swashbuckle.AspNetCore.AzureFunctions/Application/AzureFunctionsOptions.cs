239f370caf2088e9181782488aea96894079c027
John Hoerr <jhoerr@gmail.com>
2019-01-09 22:18:18 -0500

    Add AzureFunctionsOption.RoutePrefix
    This option allows for the specification of a route prefix, ala the host.json spec. Defaults to "api".

--- Swashbuckle.AspNetCore.AzureFunctions/Application/AzureFunctionsOptions.cs
+++ Swashbuckle.AspNetCore.AzureFunctions/Application/AzureFunctionsOptions.cs
@@ -5,5 +5,17 @@ namespace Swashbuckle.AspNetCore.AzureFunctions.Application
     public class AzureFunctionsOptions
     {
         public Assembly Assembly { get; set; }
+
+        /// <summary>
+        /// The route prefix that applies to all HTTP Trigger routes.
+        /// Default: "api"
+        /// </summary>
+        /// <remarks>
+        /// If you have modified the route prefix in your function's host.json 
+        /// file, it should be changed here to match. Use an empty string to
+        /// remove the default prefix.
+        /// See also: https://docs.microsoft.com/en-us/azure/azure-functions/functions-host-json#http
+        /// </remarks>
+        public string RoutePrefix { get; set; } = "api";
     }
 }
