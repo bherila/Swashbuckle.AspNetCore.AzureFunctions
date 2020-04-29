dbcae50da7fd2f98cffb241d72838f59acc222bc
Dan Friedman <dan@thefriedmans.org>
2020-04-29 15:22:47 -0500

    Reformat

--- Example/Function1.cs
+++ Example/Function1.cs
@@ -12,13 +12,13 @@ namespace Example
 {
     public static class Function1
     {
-	    [FunctionName(nameof(Run))]
-		[SwaggerOperation(nameof(Run), Tags = new[] { "Sample" })]
-		[SwaggerResponse(type: typeof(string), statusCode: 200)]
-		[SupportedMediaType("application/json")]
-	    [OptionalQueryParameter("name", typeof(string))]
-		[SwaggerOperationFilter(typeof(AppendHttpMethodToOperationIdFilter))]
-		public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        [FunctionName(nameof(Run))]
+        [SwaggerOperation(nameof(Run), Tags = new[] { "Sample" })]
+        [SwaggerResponse(type: typeof(string), statusCode: 200)]
+        [SupportedMediaType("application/json")]
+        [OptionalQueryParameter("name", typeof(string))]
+        [SwaggerOperationFilter(typeof(AppendHttpMethodToOperationIdFilter))]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
             return req.CreateResponse(HttpStatusCode.OK, "Hello");
