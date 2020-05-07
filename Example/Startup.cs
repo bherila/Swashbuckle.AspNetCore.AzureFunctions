aeee258e747533879930b8558086ca34fda5a807
Dan Friedman <dan@thefriedmans.org>
2020-05-07 09:35:09 -0500

    Remove unneeded code

--- Example/Startup.cs
+++ Example/Startup.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.AzureFunctions.Extensions;
-using System;
 using System.Reflection;
 
 [assembly: FunctionsStartup(typeof(Example.Startup))]
@@ -39,31 +37,7 @@ namespace Example
                 options.EnableAnnotations();
             });
 
-            builder.Services.AddSingleton<Microsoft.AspNetCore.Hosting.IHostingEnvironment>(s =>
-            {
-                var env = s.GetService<Microsoft.Extensions.Hosting.IHostingEnvironment>();
-                return new HostingEnvironment
-                {
-                    ApplicationName = env.ApplicationName,
-                    EnvironmentName = env.EnvironmentName,
-                    ContentRootPath = env.ContentRootPath,
-                    ContentRootFileProvider = env.ContentRootFileProvider,
-                };
-            });
-
             services.AddSwaggerGenNewtonsoftSupport();
         }
-    }
-
-    public class HostingEnvironment : Microsoft.AspNetCore.Hosting.IHostingEnvironment
-    {
-        public string EnvironmentName { get; set; }
-
-        public string WebRoot { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string ApplicationName { get; set; }
-        public string WebRootPath { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public IFileProvider WebRootFileProvider { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string ContentRootPath { get; set; }
-        public IFileProvider ContentRootFileProvider { get; set; }
     }
 }
